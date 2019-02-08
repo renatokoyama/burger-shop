6884d4c4fe9028e1d7b1269fbdd41f20e2ba4c96
Renato Koyama <renato.koyama@gmail.com>
2019-02-07 22:44:03 -0200

    Create message when the burger ingredients is empty.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.css'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map((igKey) => {
             return [
                 ...Array(props.ingredients[igKey])].map((_, index) => {
                     return <BurgerIngredient key={igKey + index} type={igKey} />
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please Start adding ingredients </p>
+    }
 
     return (
         <div className={classes.Burger}>
